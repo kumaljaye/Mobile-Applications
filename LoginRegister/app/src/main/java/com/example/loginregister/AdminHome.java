6846dbe29e17b47f408498d35f7f2358d5f400d2
kumalj <jayawardanekumal@gmail.com>
2025-07-03 17:11:40 +0530

    changes 2

--- LoginRegister/app/src/main/java/com/example/loginregister/AdminHome.java
+++ LoginRegister/app/src/main/java/com/example/loginregister/AdminHome.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminHome extends AppCompatActivity {
 
-    Button addroombtn,addofferbtn,addeventbtn;
+    Button addroombtn,addofferbtn,addeventbtn,logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class AdminHome extends AppCompatActivity {
         addroombtn = findViewById(R.id.addroombtn);
         addofferbtn = findViewById(R.id.addofferbtn);
         addeventbtn = findViewById(R.id.addeventbtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
 
         addroombtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +55,16 @@ public class AdminHome extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
+        });
+
+        logoutBtn.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(AdminHome.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+
+            // Redirect to Login screen
+            Intent intent = new Intent(AdminHome.this, Login.class); // Change to your login activity
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear backstack
+            startActivity(intent);
         });
 
     }
